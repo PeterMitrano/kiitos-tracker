82fde3fc964885c6be5b36d564e294fc94b76dca
Andrea <asipos@umich.edu>
2023-01-01 17:07:31 -0500

    integration with rescaled UI

--- main.py
+++ main.py
@@ -1,179 +1,172 @@
 import random
-import time
-
 import cv2
 import numpy as np
 import pygame
 
-from ocr import GoogleOCR
-from video_capture import CaptureManager
-
-confidence_inc = 0.25
-confidence_dec = confidence_inc / 5
-motion_alpha = 0.5
+from tracking_and_detection import NewCardDetector
 
 
-class CardTracker:
-
-    def __init__(self, letter, position):
-        self.letter = letter
-        self.position = position
-        self.confidence = 0.1
-        self.reported = False
-
-    def __repr__(self):
-        return f'{self.letter} {self.position} {self.confidence} {self.reported}'
-
-
-class NewCardDetector:
-
-    def __init__(self):
-        self.card_trackers = []
-        self.cap = cv2.VideoCapture(2)
-        self.cap_manager = CaptureManager(self.cap)
-        while self.cap_manager.last_frame is None:
-            time.sleep(1)
-
-        self.ocr = GoogleOCR()
-        # self.ocr = None
-
-    def detect(self):
-        frame = self.cap_manager.last_frame
-
-        frame = cv2.rotate(frame, cv2.ROTATE_180)
-
-        if self.ocr is not None:
-            annotated_frame, detected_letters, detected_positions = self.ocr.detect(frame)
-            cv2.imshow('OCR', annotated_frame)
-
-            for letter, position in zip(detected_letters, detected_positions):
-                tracker_found = False
-                for card_tracker in self.card_trackers:
-                    if card_tracker.letter == letter and np.linalg.norm(card_tracker.position - position) < 50:
-                        tracker_found = True
-                        break
-                if not tracker_found:
-                    self.card_trackers.append(CardTracker(letter, position))
-                else:
-                    card_tracker.position = motion_alpha * card_tracker.position + (1 - motion_alpha) * position
-                    card_tracker.confidence = min(1, card_tracker.confidence + confidence_inc)
-
-            to_remove = []
-            for card_tracker in self.card_trackers:
-                card_tracker.confidence -= confidence_dec
-                if card_tracker.confidence <= 0:
-                    to_remove.append(card_tracker)
-
-            for to_remove_i in to_remove:
-                self.card_trackers.remove(to_remove_i)
-
-            for card_tracker in self.card_trackers:
-                if card_tracker.confidence > 0.9 and not card_tracker.reported:
-                    card_tracker.reported = True
-                    return card_tracker.letter
-
-        return None
+pygame.init()
+game_width = 625
+game_height = 900
+top_padding = 100
+left_padding = 25
+n_rows = 5
+n_cols = 5
+frame_padding = 50
+card_padding = 25
+width = 75
+height = 100
+background = pygame.image.load('img/background.jpg')
+background = pygame.transform.scale(background, (game_width + 500, game_height + 500))
+headline_font = pygame.font.SysFont(None, 80)
+instruction_font = pygame.font.SysFont(None, 40)
+card_font = pygame.font.SysFont(None, 50)
 
 
-def get_card_dict():
-    return {
-        'A': 5,
-        'B': 2,
-        'C': 3,
-        'D': 3,
-        'E': 8,
-        'F': 2,
-        'G': 2,
-        'H': 2,
-        'I': 6,
-        'J': 1,
-        'K': 1,
-        'L': 3,
-        'M': 2,
-        'N': 4,
-        'O': 5,
-        'P': 2,
-        'R': 5,
-        'S': 5,
-        'T': 4,
-        'U': 2,
-        'V': 1,
-        'W': 1,
-        'X': 1,
-        'Y': 1,
-        'Z': 1,
-    }
+def reset_card_dict():
+    card_dict = dict()
+    card_dict['A'] = 5
+    card_dict['B'] = 2
+    card_dict['C'] = 3
+    card_dict['D'] = 3
+    card_dict['E'] = 8
+    card_dict['F'] = 2
+    card_dict['G'] = 2
+    card_dict['H'] = 2
+    card_dict['I'] = 6
+    card_dict['J'] = 1
+    card_dict['K'] = 1
+    card_dict['L'] = 3
+    card_dict['M'] = 2
+    card_dict['N'] = 4
+    card_dict['O'] = 5
+    card_dict['P'] = 2
+    card_dict['R'] = 5
+    card_dict['S'] = 5
+    card_dict['T'] = 4
+    card_dict['U'] = 2
+    card_dict['V'] = 1
+    card_dict['W'] = 1
+    card_dict['X'] = 1
+    card_dict['Y'] = 1
+    card_dict['Z'] = 1
+    return card_dict
 
 
-def draw_rectangles(display, top_padding, left_padding=50, n_rows=5, n_cols=5, frame_padding=100, card_padding=50,
-                    width=150, height=200):
-    frame_width = frame_padding + n_cols * width + (n_cols + 1) * card_padding
-    frame_height = frame_padding + n_rows * height + (n_rows + 1) * card_padding
+def update_cards(cards, random_update=False):
+    round_over = np.sum(np.asarray(list(cards.values()))) <= 0
+    if random_update and not round_over:
+        choice_var = random.randint(0, 99)
+        if choice_var > 90:
+            random_letter = chr(random.randint(65, 90))
+            while random_letter == 'Q' or cards[random_letter] == 0:
+                random_letter = chr(random.randint(65, 90))
+            cards[random_letter] -= 1
+    return round_over
+
+
+def draw_rectangles(display):
+    frame_width = frame_padding + n_cols*width + (n_cols + 1)*card_padding
+    frame_height = frame_padding + n_rows*height + (n_rows + 1)*card_padding
     card_frame = pygame.Rect(left_padding, top_padding, frame_width, frame_height)
     pygame.draw.rect(display, (0, 0, 255), card_frame, width=20, border_radius=80)
     for row in range(n_rows):
         for col in range(n_cols):
-            card_left = left_padding + frame_padding + col * width + col * card_padding
-            card_top = top_padding + frame_padding + row * height + row * card_padding
+            card_left = left_padding + frame_padding + col*width + col*card_padding
+            card_top = top_padding + frame_padding + row*height + row*card_padding
             card_rect = pygame.Rect(card_left, card_top, width, height)
             pygame.draw.rect(display, (0, 0, 255), card_rect)
 
 
-def draw_cards(display, font, cards, top_padding, left_padding=50, n_rows=5, n_cols=5, frame_padding=100,
-               card_padding=50, width=150, height=200):
-    for letter in cards.keys():
+def draw_cards(display, font, cards):
+    for letter in list(cards.keys()):
         letter_size = font.size(letter)
         value_size = font.size(str(cards[letter]))
         row = (ord(letter) - 65) // n_cols if ord(letter) < ord('Q') else (ord(letter) - 65 - 1) // n_cols
         col = (ord(letter) - 65) % n_rows if ord(letter) < ord('Q') else (ord(letter) - 65 - 1) % n_rows
-        card_left = left_padding + frame_padding + col * width + col * card_padding
+        card_left = left_padding + frame_padding + col*width + col*card_padding
         card_top = top_padding + frame_padding + row * height + row * card_padding
         text_left = card_left + (width - letter_size[0]) // 2
-        text_top = card_top + (height // 2 - letter_size[1]) // 2
+        text_top = card_top + (height//2 - letter_size[1]) // 2
         value_left = card_left + (width - value_size[0]) // 2
-        value_top = card_top + height // 2 + (height // 2 - value_size[1]) // 2
+        value_top = card_top + height//2 + (height//2 - value_size[1]) // 2
         letter_img = font.render(letter, True, (255, 255, 255))
         value_img = font.render(str(cards[letter]), True, (255, 255, 255))
         display.blit(letter_img, (text_left, text_top))
         display.blit(value_img, (value_left, value_top))
 
 
+def draw_board(screen, remaining_cards, round_count):
+    screen.fill((255, 255, 255))
+    screen.blit(background, (0, 0))
+    draw_rectangles(screen)
+    headline_size = headline_font.size(f'Kiitos: Round {round_count}')
+    headline = headline_font.render(f'Kiitos: Round {round_count}', True, (0, 0, 255))
+    screen.blit(headline, ((game_width - headline_size[0]) // 2, (top_padding - headline_size[1]) // 2))
+    draw_cards(screen, card_font, remaining_cards)
+    pygame.display.flip()
+
+
+def draw_round_reset(screen, remaining_cards, round_count):
+    round_over_size = instruction_font.size(f'Round {round_count} over! Reset the playing area.')
+    round_over_text = instruction_font.render(f'Round {round_count} over! Reset the playing area.', True, (0, 0, 255))
+    countdown = 5
+    interval = 1000
+    for i in range(countdown):
+        draw_board(screen, remaining_cards, round_count)
+        countdown_size = instruction_font.size(f'Starting the next round in...{countdown - i}')
+        countdown_text = instruction_font.render(f'Starting the next round in...{countdown - i}', True, (0, 0, 255))
+        screen.blit(round_over_text, ((game_width - round_over_size[0]) // 2,
+                                      game_height - top_padding + (top_padding // 2 - round_over_size[1]) // 2))
+        screen.blit(countdown_text, ((game_width - countdown_size[0]) // 2,
+                                     game_height - top_padding // 2 + (top_padding // 2 - countdown_size[1]) // 2))
+        pygame.display.flip()
+        pygame.time.wait(interval)
+    round_count += 1
+
+
+def draw_game_over(screen):
+    screen.fill((255, 255, 255))
+    screen.blit(background, (0, 0))
+    # card_frame = pygame.Rect(left_padding, top_padding, frame_width, frame_height)
+    # pygame.draw.rect(screen, (0, 0, 255), card_frame, width=20, border_radius=80)
+    pygame.display.flip()
+
+
 def run_kiitos():
     ncd = NewCardDetector()
-    random.seed(0)
-    remaining_cards = get_card_dict()
-    pygame.init()
-    game_width = 1250
-    game_height = 1800
-    top_padding = 200
-    screen = pygame.display.set_mode([game_width, game_height])
-    background = pygame.image.load('background.jpg')
-    background = pygame.transform.scale(background, (game_width + 500, game_height + 500))
-    headline_font = pygame.font.SysFont(None, 128)
-    headline_size = headline_font.size('Kiitos: Remaining letters!')
-    card_font = pygame.font.SysFont(None, 100)
-    headline = headline_font.render('Kiitos: Remaining letters!', True, (0, 0, 255))
+    remaining_cards = reset_card_dict()
 
+    screen = pygame.display.set_mode([game_width, game_height])
+
+    round_count = 1
+    game_over = False
     running = True
-    while running:
+    while running and not game_over and round_count < 4:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 print("Thanks for playing Kiitos with Peter and Andrea's card counter!")
 
-        new_card = ncd.detect()
-        if new_card is not None:
-            if new_card in remaining_cards:
-                on_new_valid_card(new_card, remaining_cards)
-            else:
-                print(f"bad detection! {new_card}")
+        # new_card = ncd.detect()
+        # if new_card is not None:
+        #     if new_card in remaining_cards:
+        #         on_new_valid_card(new_card, remaining_cards)
+        #     else:
+        #         print(f"bad detection! {new_card}")
 
-        screen.fill((255, 255, 255))
-        screen.blit(background, (0, 0))
-        screen.blit(headline, ((game_width - headline_size[0]) // 2, (top_padding - headline_size[1]) // 2))
-        draw_rectangles(screen, top_padding)
-        draw_cards(screen, card_font, remaining_cards, top_padding)
-        pygame.display.flip()
+        round_over = update_cards(remaining_cards, random_update=True)
+        draw_board(screen, remaining_cards, round_count)
+
+        if round_over:
+            draw_round_reset(screen, remaining_cards, round_count)
+            remaining_cards = reset_card_dict()
+
+        if round_over and round_count == 3:
+            game_over = True
+            draw_game_over(screen)
+            pygame.time.wait(5000)
 
         cv2.waitKey(10)
 
---
+++ tracking_and_detection.py
@@ -0,0 +1,69 @@
+from ocr import GoogleOCR
+from video_capture import CaptureManager
+import cv2
+import time
+
+confidence_inc = 0.25
+confidence_dec = confidence_inc / 5
+motion_alpha = 0.5
+
+class CardTracker:
+
+    def __init__(self, letter, position):
+        self.letter = letter
+        self.position = position
+        self.confidence = 0.1
+        self.reported = False
+
+    def __repr__(self):
+        return f'{self.letter} {self.position} {self.confidence} {self.reported}'
+
+
+class NewCardDetector:
+
+    def __init__(self):
+        self.card_trackers = []
+        self.cap = cv2.VideoCapture(2)
+        self.cap_manager = CaptureManager(self.cap)
+        while self.cap_manager.last_frame is None:
+            time.sleep(1)
+
+        self.ocr = GoogleOCR()
+        # self.ocr = None
+
+    def detect(self):
+        frame = self.cap_manager.last_frame
+
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+        if self.ocr is not None:
+            annotated_frame, detected_letters, detected_positions = self.ocr.detect(frame)
+            cv2.imshow('OCR', annotated_frame)
+
+            for letter, position in zip(detected_letters, detected_positions):
+                tracker_found = False
+                for card_tracker in self.card_trackers:
+                    if card_tracker.letter == letter and np.linalg.norm(card_tracker.position - position) < 50:
+                        tracker_found = True
+                        break
+                if not tracker_found:
+                    self.card_trackers.append(CardTracker(letter, position))
+                else:
+                    card_tracker.position = motion_alpha * card_tracker.position + (1 - motion_alpha) * position
+                    card_tracker.confidence = min(1, card_tracker.confidence + confidence_inc)
+
+            to_remove = []
+            for card_tracker in self.card_trackers:
+                card_tracker.confidence -= confidence_dec
+                if card_tracker.confidence <= 0:
+                    to_remove.append(card_tracker)
+
+            for to_remove_i in to_remove:
+                self.card_trackers.remove(to_remove_i)
+
+            for card_tracker in self.card_trackers:
+                if card_tracker.confidence > 0.9 and not card_tracker.reported:
+                    card_tracker.reported = True
+                    return card_tracker.letter
+
+        return None
