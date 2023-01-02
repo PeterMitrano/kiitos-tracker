712a5ee9d926eb34f65b047fbe44f3b5d2c540e1
Andrea <asipos@umich.edu>
2023-01-02 11:27:35 -0500

    updated GUI

--- main.py
+++ main.py
@@ -18,11 +18,17 @@ frame_padding = 50
 card_padding = 25
 width = 75
 height = 100
+countdown = 3
+interval = 500
 background = pygame.image.load('img/background.jpg')
 background = pygame.transform.scale(background, (game_width + 500, game_height + 500))
 headline_font = pygame.font.SysFont(None, 80)
 instruction_font = pygame.font.SysFont(None, 40)
 card_font = pygame.font.SysFont(None, 50)
+WHITE = (255, 255, 255)
+GRAY = (199, 201, 194)
+BLACK = (0, 0, 0)
+BLUE = (38, 47, 78)
 
 
 def reset_card_dict():
@@ -59,13 +65,13 @@ def draw_rectangles(display):
     frame_width = frame_padding + n_cols * width + (n_cols + 1) * card_padding
     frame_height = frame_padding + n_rows * height + (n_rows + 1) * card_padding
     card_frame = pygame.Rect(left_padding, top_padding, frame_width, frame_height)
-    pygame.draw.rect(display, (0, 0, 255), card_frame, width=20, border_radius=80)
+    pygame.draw.rect(display, BLUE, card_frame, width=20, border_radius=80)
     for row in range(n_rows):
         for col in range(n_cols):
             card_left = left_padding + frame_padding + col * width + col * card_padding
             card_top = top_padding + frame_padding + row * height + row * card_padding
             card_rect = pygame.Rect(card_left, card_top, width, height)
-            pygame.draw.rect(display, (0, 0, 255), card_rect)
+            pygame.draw.rect(display, BLUE, card_rect, border_radius=20)
 
 
 def draw_cards(display, font, cards):
@@ -80,18 +86,18 @@ def draw_cards(display, font, cards):
         text_top = card_top + (height // 2 - letter_size[1]) // 2
         value_left = card_left + (width - value_size[0]) // 2
         value_top = card_top + height // 2 + (height // 2 - value_size[1]) // 2
-        letter_img = font.render(letter, True, (255, 255, 255))
-        value_img = font.render(str(cards[letter]), True, (255, 255, 255))
+        letter_img = font.render(letter, True, GRAY)
+        value_img = font.render(str(cards[letter]), True, WHITE)
         display.blit(letter_img, (text_left, text_top))
         display.blit(value_img, (value_left, value_top))
 
 
 def draw_board(screen, remaining_cards, round_count):
-    screen.fill((255, 255, 255))
+    screen.fill(WHITE)
     screen.blit(background, (0, 0))
     draw_rectangles(screen)
     headline_size = headline_font.size(f'Kiitos: Round {round_count}')
-    headline = headline_font.render(f'Kiitos: Round {round_count}', True, (0, 0, 255))
+    headline = headline_font.render(f'Kiitos: Round {round_count}', True, BLUE)
     screen.blit(headline, ((game_width - headline_size[0]) // 2, (top_padding - headline_size[1]) // 2))
     draw_cards(screen, card_font, remaining_cards)
     pygame.display.flip()
@@ -99,13 +105,12 @@ def draw_board(screen, remaining_cards, round_count):
 
 def draw_round_reset(screen, remaining_cards, round_count):
     round_over_size = instruction_font.size(f'Round {round_count} over! Reset the playing area.')
-    round_over_text = instruction_font.render(f'Round {round_count} over! Reset the playing area.', True, (0, 0, 255))
-    countdown = 5
-    interval = 1000
+    round_over_text = instruction_font.render(f'Round {round_count} over! Reset the playing area.', True, BLUE)
+
     for i in range(countdown):
         draw_board(screen, remaining_cards, round_count)
         countdown_size = instruction_font.size(f'Starting the next round in...{countdown - i}')
-        countdown_text = instruction_font.render(f'Starting the next round in...{countdown - i}', True, (0, 0, 255))
+        countdown_text = instruction_font.render(f'Starting the next round in...{countdown - i}', True, BLUE)
         screen.blit(round_over_text, ((game_width - round_over_size[0]) // 2,
                                       game_height - top_padding + (top_padding // 2 - round_over_size[1]) // 2))
         screen.blit(countdown_text, ((game_width - countdown_size[0]) // 2,
@@ -115,10 +120,14 @@ def draw_round_reset(screen, remaining_cards, round_count):
 
 
 def draw_game_over(screen):
-    screen.fill((255, 255, 255))
+    screen.fill(GRAY)
     screen.blit(background, (0, 0))
-    # card_frame = pygame.Rect(left_padding, top_padding, frame_width, frame_height)
-    # pygame.draw.rect(screen, (0, 0, 255), card_frame, width=20, border_radius=80)
+    text_size = headline_font.size('Game Over!')
+    text = headline_font.render('Game Over!', True, WHITE)
+    text_padding = 40
+    text_background = pygame.Rect(game_width // 2 - (text_size[0] + text_padding) // 2, game_height // 2 - (text_size[1] + text_padding) // 2, text_size[0] + text_padding, text_size[1] + text_padding)
+    pygame.draw.rect(screen, BLUE, text_background, border_radius=40)
+    screen.blit(text, (game_width // 2 - (text_size[0]) // 2, game_height // 2 - (text_size[1]) // 2))
     pygame.display.flip()
 
 
