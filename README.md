a6f656ee2d2ee1dc3a9e170c33b610ca4b8343e7
Peter <peter@einstein>
2023-01-22 18:51:09 -0700

    make saving/uploading for labeling asynchronous

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Kiitos Card Counter
 
 Powered by convolution neural networks!
+
+### TODO:
+
+- when the user "saves" the image
+   - upload to google drive
+   - added to labelbox dataset
+- see how well it works if we train only on real images, no synthetic data
+
+|              | with synthetic | without synthetic |
+|--------------|----------------|-------------------|
+| num mistakes | 3              | 3                 |
+
+
+That's so interesting. It took 500 epochs instead of 25, but that seems to match the fact that there were far fewer examples overall.
--- src/kiitos/main.py
+++ src/kiitos/main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from PIL import Image
 # noinspection PyUnresolvedReferences
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import QLibraryInfo, QThread
+from PyQt5.QtCore import QLibraryInfo, QThread, QObject
 from PyQt5.QtCore import Qt
 
 from counts_widget import CountsWidget
@@ -19,6 +19,23 @@ from kiitos.upload_for_labeling import make_labelbox_client, upload_image_to_buc
 # This is a problem caused by OpenCV
 # https://stackoverflow.com/questions/68417682/
 os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = QLibraryInfo.location(QLibraryInfo.PluginsPath)
+
+
+class CaptureWorker(QObject):
+
+    def __init__(self, capture_manager):
+        super().__init__()
+        self.capture_manager = capture_manager
+
+    def save_last_frame(self):
+        pil_img = Image.fromarray(self.capture_manager.last_frame)
+        pil_img = pil_img.rotate(180)
+        image_path = pathlib.Path(f'saved_from_live/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png')
+        pil_img.save(image_path)
+        labelbox_client = make_labelbox_client()
+        url = upload_image_to_bucket(image_path)
+        upload_to_labelbox(labelbox_client, url)
+        print(f"Saved image to {image_path}, and uploaded it for labeling")
 
 
 class KiitosUi(QtWidgets.QMainWindow):
@@ -54,6 +71,8 @@ class KiitosUi(QtWidgets.QMainWindow):
         self.camera_thread.finished.connect(self.camera_thread.deleteLater)
         self.camera_thread.start()
 
+        self.capture_worker = CaptureWorker(self.detector_widget.ncd.cap_manager)
+
         self.show()
 
     def on_next_round(self):
@@ -66,14 +85,11 @@ class KiitosUi(QtWidgets.QMainWindow):
         self.counts_widget.update()
 
     def save_last_frame(self, _):
-        pil_img = Image.fromarray(self.detector_widget.ncd.cap_manager.last_frame)
-        pil_img = pil_img.rotate(180)
-        image_path = pathlib.Path(f'saved_from_live/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.png')
-        pil_img.save(image_path)
-        labelbox_client = make_labelbox_client()
-        url = upload_image_to_bucket(image_path)
-        upload_to_labelbox(labelbox_client, url)
-        print(f"Saved image to f{image_path}, and uploaded it for labeling")
+        thread = QThread()
+        self.capture_worker.moveToThread(thread)
+        thread.started.connect(self.capture_worker.save_last_frame)
+        thread.finished.connect(thread.deleteLater)
+        thread.start()
 
     def new_detection(self):
         self.counts_widget.update()
